2b53bd475e20548bb1d5494f47ce2b2bf57b3e1e
Aditya Gorane <168202112+AdiGo112@users.noreply.github.com>
2025-12-24 22:34:06 +0530

    new template for CF problem solutions

---
+++ CF/2_template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPEDEFS ====================//
+using ll = long long;
+using pii = pair<ll,ll>;
+using vi = vector<ll>;
+using vpi = vector<pii>;
+
+//==================== MACROS =====================//
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define sz(x) ((int)(x.size()))
+
+//================== FAST I/O ===================//
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+//================== DEBUGGING ==================//
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << "\n"
+#else
+#define debug(x)
+#endif
+
+//=============== COMMON FUNCTIONS ==============//
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+//================ SOLVE FUNCTION =================//
+
+void solve() {
+    
+}
+
+//==================== MAIN ======================//
+int main() {
+    fastio;
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
