197dafb1c31273ce302129309a3f4679275a4f23
Aditya Gorane <168202112+AdiGo112@users.noreply.github.com>
2026-01-18 13:11:45 +0530

    +33 beutiful

---
+++ CF/17-01-26/A.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPEDEFS ====================//
+using ll  = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pii>;
+using vpl = vector<pll>;
+
+//==================== CONSTANTS ==================//
+const ll INFLL = (ll)1e18;
+const int INF  = (int)1e9;
+const int MOD  = 1e9 + 7;
+
+//==================== MACROS =====================//
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (int)((x).size())
+#define pb      push_back
+
+//==================== FAST I/O ===================//
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+//==================== DEBUG ======================//
+#ifdef LOCAL
+    #define debug(x) cerr << "[DEBUG] " << #x << " = " << x << "\n"
+    #define debugv(v) do { \
+        cerr << "[DEBUG] " << #v << " = "; \
+        for (auto &e : v) cerr << e << " "; \
+        cerr << "\n"; \
+    } while(0)
+#else
+    #define debug(x)
+    #define debugv(v)
+#endif
+
+//==================== MATH UTILS =================//
+ll mod_add(ll a, ll b) {
+    a %= MOD; b %= MOD;
+    return (a + b) % MOD;
+}
+
+ll mod_sub(ll a, ll b) {
+    a %= MOD; b %= MOD;
+    return (a - b + MOD) % MOD;
+}
+
+ll mod_mul(ll a, ll b) {
+    a %= MOD; b %= MOD;
+    return (a * b) % MOD;
+}
+
+ll mod_pow(ll a, ll p) {
+    ll res = 1;
+    while (p) {
+        if (p & 1) res = mod_mul(res, a);
+        a = mod_mul(a, a);
+        p >>= 1;
+    }
+    return res;
+}
+
+//==================== INPUT / OUTPUT =================//
+
+// input for vector (size must already be set)
+template <typename T>
+istream& operator>>(istream& in, vector<T>& v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+// output for vector
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (int i = 0; i < (int)v.size(); i++) {
+        if (i) out << ' ';
+        out << v[i];
+    }
+    return out;
+}
+//==================== PERMUTATION FUNCTION ====================//
+string nthPermutation(string s, int k) {
+    vector<char> nums(s.begin(), s.end());
+    int n = nums.size();
+
+    vector<int> fact(n);
+    fact[0] = 1;
+    for(int i = 1; i < n; i++)
+        fact[i] = fact[i-1] * i;
+
+    k--; // zero-based
+
+    string ans = "";
+
+    for(int i = n; i >= 1; i--) {
+        int idx = k / fact[i-1];
+        ans += nums[idx];
+        nums.erase(nums.begin() + idx);
+        k %= fact[i-1];
+    }
+
+    return ans;
+}
+//==================== SOLVE ======================//
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+
+    for(int i = 0; i < n - 1; i++){
+        if(a[i] % 2 == a[i+1]%2){
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+
+//==================== MAIN =======================//
+int main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ CF/17-01-26/C.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//==================== TYPEDEFS ====================//
+using ll  = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pii>;
+using vpl = vector<pll>;
+
+//==================== CONSTANTS ==================//
+const ll INFLL = (ll)1e18;
+const int INF  = (int)1e9;
+const int MOD  = 1e9 + 7;
+
+//==================== MACROS =====================//
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (int)((x).size())
+#define pb      push_back
+
+//==================== FAST I/O ===================//
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+//==================== DEBUG ======================//
+#ifdef LOCAL
+    #define debug(x) cerr << "[DEBUG] " << #x << " = " << x << "\n"
+    #define debugv(v) do { \
+        cerr << "[DEBUG] " << #v << " = "; \
+        for (auto &e : v) cerr << e << " "; \
+        cerr << "\n"; \
+    } while(0)
+#else
+    #define debug(x)
+    #define debugv(v)
+#endif
+
+//==================== MATH UTILS =================//
+ll mod_add(ll a, ll b) {
+    a %= MOD; b %= MOD;
+    return (a + b) % MOD;
+}
+
+ll mod_sub(ll a, ll b) {
+    a %= MOD; b %= MOD;
+    return (a - b + MOD) % MOD;
+}
+
+ll mod_mul(ll a, ll b) {
+    a %= MOD; b %= MOD;
+    return (a * b) % MOD;
+}
+
+ll mod_pow(ll a, ll p) {
+    ll res = 1;
+    while (p) {
+        if (p & 1) res = mod_mul(res, a);
+        a = mod_mul(a, a);
+        p >>= 1;
+    }
+    return res;
+}
+
+//==================== INPUT / OUTPUT =================//
+
+// input for vector (size must already be set)
+template <typename T>
+istream& operator>>(istream& in, vector<T>& v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+// output for vector
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (int i = 0; i < (int)v.size(); i++) {
+        if (i) out << ' ';
+        out << v[i];
+    }
+    return out;
+}
+//==================== PERMUTATION FUNCTION ====================//
+string nthPermutation(string s, int k) {
+    vector<char> nums(s.begin(), s.end());
+    int n = nums.size();
+
+    vector<int> fact(n);
+    fact[0] = 1;
+    for(int i = 1; i < n; i++)
+        fact[i] = fact[i-1] * i;
+
+    k--; // zero-based
+
+    string ans = "";
+
+    for(int i = n; i >= 1; i--) {
+        int idx = k / fact[i-1];
+        ans += nums[idx];
+        nums.erase(nums.begin() + idx);
+        k %= fact[i-1];
+    }
+
+    return ans;
+}
+//==================== SOLVE ======================//
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+
+    // Check if already sorted
+    bool sorted = true;
+    for(int i=1;i<n;i++){
+        if(s[i]<s[i-1]) sorted=false;
+    }
+    if(sorted){
+        cout<<"Bob\n";
+        return;
+    }
+
+    vector<int> ans;
+    int l = 0, r = n-1;
+
+    while(l < r){
+        while(l < r && s[l] != '1') l++;
+        while(l < r && s[r] != '0') r--;
+
+        if(l < r){
+            ans.push_back(l);
+            ans.push_back(r);
+            l++;
+            r--;
+        }
+    }
+    sort(all(ans));
+    cout<<"Alice\n";
+    cout<<ans.size()<<"\n";
+    for(int x: ans) cout<<x+1<<" ";
+    cout<<"\n";
+}
+
+//==================== MAIN =======================//
+int main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
